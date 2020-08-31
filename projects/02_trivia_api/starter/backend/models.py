474ce960f7e6398d709f8b130b435fec625a8317
george ghobrial <georgeheshmet@gmail.com>
2020-08-31 23:22:12 +0200

    getting started

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -2,9 +2,9 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
-
+from flask_migrate import Migrate
 database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = "postgres://{}@{}/{}".format('postgres', 'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -17,8 +17,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
-
+    migrate = Migrate(app,db)
 '''
 Question
 
@@ -29,7 +28,7 @@ class Question(db.Model):
   id = Column(Integer, primary_key=True)
   question = Column(String)
   answer = Column(String)
-  category = Column(String)
+  category = db.Column(db.Integer, db.ForeignKey('categories.id'))
   difficulty = Column(Integer)
 
   def __init__(self, question, answer, category, difficulty):
@@ -67,12 +66,12 @@ class Category(db.Model):
 
   id = Column(Integer, primary_key=True)
   type = Column(String)
-
+  questionz=db.relationship("Question",  backref="Categori")
   def __init__(self, type):
     self.type = type
 
   def format(self):
     return {
       'id': self.id,
-      'type': self.type
+      'category': self.type
     }
